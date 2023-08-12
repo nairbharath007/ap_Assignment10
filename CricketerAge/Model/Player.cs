ef7993537f6b9d5d9cfe225494b2f21e93af9453
Bharath Nair <nairbharath007@gmail.com>
2023-08-12 20:58:55 +0530

    predefined 4 objects added

---
+++ CricketerAge/Model/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketerAge.Model
+{
+    internal class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Player(int id, string name, int age) 
+        {
+            Id = id; 
+            Name = name;
+            Age = age;
+        }
+        public string ShowCustomerDetails()
+        {
+            return $"Id : {Id}\n Name : {Name}\n Age : {Age} ";
+        }
+        public static Player WhoIsElder(Player[] players)
+        {
+            int maxAge = players[0].Age;
+            Player maxPlayer = null;
+            for (int i = 0; i < players.Length; i++) 
+            {
+                if (players[i].Age > maxAge)
+                {
+                    maxAge = players[i].Age;
+                    maxPlayer = players[i];
+                }
+            }
+            return maxPlayer;
+        }
+    }
+}
--- CricketerAge/Program.cs
+++ CricketerAge/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CricketerAge.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,24 @@ namespace CricketerAge
     {
         static void Main(string[] args)
         {
+            Player player1 = new Player(23, "Michael Jordan", 40);
+            Player player2 = new Player(11, "Sunil Chhetri", 39);
+
+            Player[] players =
+            {
+                player1,
+                player2,
+                new Player(18, "Virat Kohli", 34),
+                new Player(7,"MS Dhoni", 42)
+            };
+
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player.ShowCustomerDetails());
+                Console.WriteLine("----------------------------------------");
+            }
+
+            Console.WriteLine($"Eldest player details: \n{Player.WhoIsElder(players).ShowCustomerDetails()}");
         }
     }
 }
